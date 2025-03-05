59ca451951a078c32e699b616496dd3f283a1dd3
Komal Dhanuka <komaldhanuka25@gmail.com>
2025-03-05 21:25:24 +0530

    netlify

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
+// const express = require('express');
+// const mongoos = require('mongoose');
+// const bodyParser = require('body-parser');
+// const homeRoutes = require('./routers/home');
+
+// const app = express();
+// const port =3002;
+
+// mongoos.connect("mongodb://localhost:27017/studentdetails", {useNewUrlParser : true})
+// const db = mongoos.connection;
+// db.on('eror',()=>{
+//     console.log("Err is ");
+// })
+// db.once('open',()=>{
+//     console.log("Connected");
+// })
+
+// app.set('view engine','ejs');
+// app.use(express.static('public'))
+
+// // body parser 
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+// app.use(bodyParser.json())
+
+
+
+// app.use('/' , homeRoutes)
+
+// app.listen(port)
+
+
+
 const express = require('express');
-const mongoos = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const homeRoutes = require('./routers/home');
+const serverless = require('serverless-http');  // Import serverless-http
 
 const app = express();
-const port =3002;
+const port = process.env.PORT || 3002; // Netlify assigns the PORT dynamically
 
-mongoos.connect("mongodb://localhost:27017/studentdetails", {useNewUrlParser : true})
-const db = mongoos.connection;
-db.on('eror',()=>{
-    console.log("Err is ");
-})
-db.once('open',()=>{
+mongoose.connect("mongodb://localhost:27017/studentdetails", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log("Error is ");
+});
+db.once('open', () => {
     console.log("Connected");
-})
+});
 
-app.set('view engine','ejs');
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-// body parser 
-app.use(bodyParser.urlencoded({ extended: false }))
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// parse application/json
-app.use(bodyParser.json())
+app.use('/', homeRoutes);
 
-
-
-app.use('/' , homeRoutes)
-
-app.listen(port)
+// Wrap your app with serverless-http
+module.exports.handler = serverless(app);
